a263acf30f79f22566adbd4bb580ceccac635cf5
OleksandrNovyk <novykav@gmail.com>
2023-04-28 16:29:04 +0300

    4

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -9,7 +9,7 @@ export const FriendListItem = ({ avatar, name, isOnline }) => {
       ) : (
         <span className={css.statusOffline}></span>
       )}
-      <img className={css.avatar} src={avatar} alt={name} width="36" />
+      <img className={css.avatar} src={avatar} alt={name} width="48" />
       <p className={css.name}>{name}</p>
     </li>
   );
