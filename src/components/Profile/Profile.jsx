1aa5558468e675018d2c11183d30e2adc0e9207d
OleksandrNovyk <novykav@gmail.com>
2023-04-28 15:25:44 +0300

    upd

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,7 +6,7 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
     <div className={css.profile}>
       <div className={css.description}>
-        <img  alt={username} className={css.avatar} />
+        <img src={avatar} alt={username} className={css.avatar} />
         <p className={css.name}>{username}</p>
         <p className={css.tag}>@{tag}</p>
         <p className={css.location}>{location}</p>
